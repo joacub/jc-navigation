bdf164e8e742ca441c66aeb56373fd7b532cdb0f
Johan <joacub@gmail.com>
2014-02-05 21:05:53 +0100

    debug
    Signed-off-by: Johan <joacub@gmail.com>

--- src/JcNavigation/Navigation/Navigation.php
+++ src/JcNavigation/Navigation/Navigation.php
@@ -82,6 +82,7 @@ class Navigation extends DefaultNavigationFactory
 		                    if($ip == '85.251.56.96') {
 		                        echo '<pre>' . $view->serverUrl() . ($detector ?  '/'.  \Locale::getDefault() : '') . $view->basePath($url) . '</pre>';
 		                        $url = (strpos($url, "http://") === 0 || strpos($url, "https://") === 0 ? $url : $view->serverUrl() . ($detector ?  '/'.  \Locale::getDefault() : '') . $view->basePath($url));
+		                        $url = ($detector ? $detector->assemble(\Locale::getDefault(), $url)->toString() : $url);
 		                        echo '<pre>' . $url . '</pre>';
                             exit;
 		                    }
