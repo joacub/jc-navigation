d368262ac7a750b1abfffff31c0c6aef7b3cebd3
Johan <joacub@gmail.com>
2014-02-05 20:56:24 +0100

    cambios
    Signed-off-by: Johan <joacub@gmail.com>

--- src/JcNavigation/Navigation/Navigation.php
+++ src/JcNavigation/Navigation/Navigation.php
@@ -70,11 +70,11 @@ class Navigation extends DefaultNavigationFactory
                         
                         if(substr($url, 0, 1) == '/') {
                             $url = substr($url, 1);
-                            if(($view->serverUrl() . ($detector ?  '/'.  \Locale::getDefault() : '') . $view->basePath($url)) == 'http://www.tranviasdezaragoza.es//es/informacion/contacto') {
-                                echo '<pre>' . $view->serverUrl() . ($detector ?  '/'.  \Locale::getDefault() : '') . $view->basePath($url) . '</pre>';
-                                exit;
-                            }
-                           
+                        }
+                        
+                        if(($view->serverUrl() . ($detector ?  '/'.  \Locale::getDefault() : '') . $view->basePath($url)) == 'http://www.tranviasdezaragoza.es//es/informacion/contacto') {
+                            echo '<pre>' . $view->serverUrl() . ($detector ?  '/'.  \Locale::getDefault() : '') . $view->basePath($url) . '</pre>';
+                            exit;
                         }
                         
                         $url = (strpos($url, "http://") === 0 || strpos($url, "https://") === 0 ? $url : $view->serverUrl() . ($detector ?  '/'.  \Locale::getDefault() : '') . $view->basePath($url));
